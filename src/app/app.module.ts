7db1ad79280fc1856a1e9ccbd3a83acaadbc21c8
Irina Skh <irina.sukhorukova@etu.unice.fr>
2023-01-05 12:05:39 +0100

    Début de travail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormField } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { RenduDirective } from './shared/rendu.directive';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+
+
+// import {Component} from '@angular/core';
+
+// /**
+//  * @title Basic buttons
+//  */
+// @Component({
+//   selector: 'button-overview-example',
+//   templateUrl: 'button-overview-example.html',
+//   styleUrls: ['button-overview-example.css'],
+// })
+// export class ButtonOverviewExample {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    RenduDirective
+    //MatFormFieldModule,
+    //MatInputModule
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    FormsModule,
+    //MatFormField,
+    MatInputModule,
+    MatDatepickerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
